14767eb1ccf8e68fa388ab5d9bb74517902c59a4
EC2 Default User <ec2-user@ip-172-31-65-50.ec2.internal>
2020-11-25 05:06:15 +0000

    initial commit of node.js version of hw1

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require("express");
 const app = express();
 const faker = require('faker');
-var fakeEmail = faker.internet.email();
-var fakePhone = faker.phone.phoneNumber();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
     var fakeData = ["", ""];
-    fakeData[0]  = fakeEmail;
-    fakeData[1] = fakePhone;
+    fakeData[0]  = faker.internet.email();;
+    fakeData[1] = faker.phone.phoneNumber();
     res.render("index.ejs", {fakeData:fakeData});
 });
 
 app.get("/ip", function(req, res){
     var fakeData = ["", ""];
-    fakeData[0]  = fakeEmail;
-    fakeData[1] = fakePhone;
+    fakeData[0]  = faker.internet.email();;
+    fakeData[1] = faker.phone.phoneNumber();
     res.render("ip.ejs", {fakeData:fakeData});
 });
     
 app.get("/routes", function(req, res){
     var fakeData = ["", ""];
-    fakeData[0]  = fakeEmail;
-    fakeData[1] = fakePhone;
+    fakeData[0]  = faker.internet.email();;
+    fakeData[1] = faker.phone.phoneNumber();
     res.render("routes.ejs", {fakeData:fakeData});
 });
 
 app.get("/switch", function(req, res){
     var fakeData = ["", ""];
-    fakeData[0]  = fakeEmail;
-    fakeData[1] = fakePhone;
+    fakeData[0]  = faker.internet.email();;
+    fakeData[1] = faker.phone.phoneNumber();
     res.render("switch.ejs", {fakeData:fakeData});
 });
 
